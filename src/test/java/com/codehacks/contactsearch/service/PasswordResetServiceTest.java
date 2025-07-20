b145b468bc14dea32019a9292405b5f87c2ab56c
Rume7 <electengrhume@gmail.com>
2025-07-20 21:18:41 +0100

    Added password reset service.

--- src/test/java/com/codehacks/contactsearch/service/PasswordResetServiceTest.java
+++ src/test/java/com/codehacks/contactsearch/service/PasswordResetServiceTest.java
@@ -5,9 +5,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.time.LocalDateTime;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(MockitoExtension.class)
 class PasswordResetServiceTest {
